e76b9a0e3155d1e7ffb48fa08455ce18a6bceb52
Rick Waldron <waldron.rick@gmail.com>
2018-02-23 12:13:28 -0500

    runtime/jsc: update to use JavaScriptCore's built-in $/$262
    https://github.com/WebKit/webkit/blob/aeed7ff2493053fb9ffcc06fedc7e75311bacc8a/Source/JavaScriptCore/jsc.cpp#L574-L604
    
    This enables tests that require $262.agent.*

--- runtimes/jsc.js
+++ runtimes/jsc.js
@@ -1,42 +1,12 @@
+/* JavaScriptCore exposes a "$" object to its runtime */
+/* Using this["\x24"]; prevents overwrite by ConsoleAgent */
+var jsc = this["\x24"];
 var $ = {
-  global: this,
-  createRealm(options) {
-    options = options || {};
-    options.globals = options.globals || {};
-
-    var realm;
-    run(this.file, function(newRealm) {
-      realm = newRealm;
-    });
-    realm.eval(this.source);
-    realm.$.source = this.source;
-    realm.$.destroy = function () {
-      if (options.destroy) {
-        options.destroy();
-      }
-    };
-
-    for(var glob in options.globals) {
-      realm.$.global[glob] = options.globals[glob];
-    }
-
-    return realm.$;
-  },
-  evalScript(code, errorCb) {
-    try {
-      print(this.scriptStartMarker);
-      print(code);
-      print(this.scriptEndMarker);
-
-      /* Blocks until the script is written to the file system. */
-      readline();
-
-      load(this.scriptFile);
-      return { type: 'normal', value: undefined };
-    } catch (e) {
-      return { type: 'throw', value: e }
-    }
-  },
+  agent: jsc.agent,
+  global: jsc.global,
+  createRealm: jsc.createRealm,
+  detachArrayBuffer: jsc.detachArrayBuffer,
+  evalScript: jsc.evalScript,
   getGlobal(name) {
     return this.global[name];
   },
